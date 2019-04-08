5eb5d00689d1f67938d332fb11243ddc75d7ba09
Marcela Pagoada <mpagoada@westinghousehs.org>
2019-04-08 22:24:51 +0000

    Day 1

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+var linkUrl = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+
 $("#search-button").click(function(){
-  
-  
-  
+  $.ajax({
+       url: linkUrl,
+       method: "GET",
+       success: function(response){
+           $("body").display(response.images.original);
+       }
+    }); 
 });
 
