d7842ca2f6bbb169aced7985e2a185d396cb13c1
Marcela Pagoada <mpagoada@westinghousehs.org>
2019-04-15 22:20:41 +0000

    Unit 6 Session 2 work

--- js/script.js
+++ js/script.js
@@ -4,14 +4,21 @@
 /* global $ */
 
 var linkUrl = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+var original = "https://media1.giphy.com/media/Y4pAQv58ETJgRwoLxj/giphy_s.gif?cid=e1bb72ff5cb49f73542e6e7251e5dc67";
+
 
 $("#search-button").click(function(){
   $.ajax({
        url: linkUrl,
        method: "GET",
        success: function(response){
-           $("body").display(response.images.original);
+           $('#original').append('<img src="' + original + '">');
+           
+           var searchTerm = $("#search-term").val();
+           var requestUrl = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
+           $('#original').append('<img src="' + requestUrl + '">');
        }
     }); 
+    
 });
 
